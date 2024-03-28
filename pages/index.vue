19290983276245dd2449623e0b91acef7da393cf
rogerwaldvogel <roger.waldvogel@holoai.ch>
2024-03-28 16:00:42 +0100

    Route examples.

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,10 @@
   <NuxtLayout name="default-layout">
     <!-- Komponente benötigt kein import in script. Wird automatisch von nuxt importiert -->
     <header-example></header-example>
+    <div>
+      <div><NuxtLink to="/page/1">Page 1</NuxtLink></div>
+      <div><NuxtLink to="/page/2">Page 2</NuxtLink></div>
+    </div>
     <UCard id="data-table-container">
       <UTable id="data-table" :rows="store.tableData"/>
     </UCard>
---
+++ pages/page/[id].vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="route-id">
+    Page {{ route.params.id }}
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const route = useRoute()
+
+</script>
+
+<style scoped lang="css">
+#route-id {
+  padding: 8px;
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
