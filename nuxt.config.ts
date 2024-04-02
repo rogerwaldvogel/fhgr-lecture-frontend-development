a2b3a598fab3a1b4b9f2954faa53506bf7a00dc1
rogerwaldvogel <roger.waldvogel@holoai.ch>
2024-04-02 10:01:27 +0200

    Added google fonts example

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true},
-    modules: ['@nuxt/ui', '@nuxtjs/supabase', '@pinia/nuxt'],
+    modules: ['@nuxt/ui', '@nuxtjs/supabase', '@pinia/nuxt', '@nuxtjs/google-fonts'],
     css: ['~/assets/css/main.css'],
     supabase: {
         redirectOptions: {
             login: '/login',
             callback: '/confirm',
             exclude: ['/*'],
+        }
+    },
+    // https://fonts.google.com/
+    googleFonts: {
+        families: {
+            // https://google-fonts.nuxtjs.org/getting-started/options#families
+            'Workbench': true,
+            Lato: [100, 300],
         }
     }
 })
